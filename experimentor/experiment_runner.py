9c0c45873acc66037f76229dcdf924257108832d
Lau Yee-Yu <liuyiyu2003@126.com>
2024-11-18 16:40:56 -0800

    refactor: change the behaviour of the SimpleCommandRunner
    The behaviour is more reasonable but more complicated, see the comment in
    the `run_experiment` method of class `SimpleCommandRunner` in
    experiment_runner.py for more details.

--- experimentor/experiment_runner.py
+++ experimentor/experiment_runner.py
@@ -21,18 +21,33 @@ class SimpleCommandRunner(BaseExperimentRunner):
         self.base_command = base_command
 
     def run_experiment(self, title: str, config: dict, file: str | None):
-        """
-        Run the experiment with command line.
+        """Run the experiment with command line.
+
+        The options will be generated according to the type of the value
+        in the configuration. Only the values in the `config` will be used.
+        1) If the value is a dictionary, options will be generated according
+           to the key-value pairs in the dictionary. If the key is a single
+           character, the option will be a short option. Otherwise, it will
+           be a long option.
+        2) If the value is not a dictionary, the command will be appended with
+           the value. The order of the values will be the same as the order of
+           the key-value pairs in the dictionary.
+
         :param title: The title of the experiment.
         :param config: The configuration of the experiment.
-        :param file: The file to store the output. If None, there's nowhere to store the output.
+        :param file: The file to store the output. If None, the output will
+            be treated as a standard output.
         """
         command = f'{self.base_command}'
-        for key, value in config.items():
-            if len(key) == 1: # short option
-                command += f' -{key} {value}'
+        for _, value in config.items():
+            if type(value) == dict:
+                for k, v in value.items():
+                    if len(k) == 1:
+                        command += f' -{k} {v}'
+                    else:
+                        command += f' --{k} {v}'
             else:
-                command += f' --{key} {value}'
+                command += f' {value}'
         if file is None:
             result = subprocess.run(command, shell=True)
         else:
