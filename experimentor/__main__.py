ec3b617c50e459e54c33113ed0b4cd6eb5463f5b
Lau Yee-Yu <liuyiyu2003@126.com>
2024-11-23 02:08:46 -0800

    feat: show full usage if there's no parameter

--- experimentor/__main__.py
+++ experimentor/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import sys
 
 from .const import DEFAULT_MAX_TRIALS
 from .experiment_runner import SimpleCommandRunner
@@ -18,6 +19,9 @@ def main():
     log_group.add_argument('--log-dir', type=str, help='Log directory')
     parser.add_argument('--max-trial', type=int,
                         default=DEFAULT_MAX_TRIALS, help='Maximum number of trials')
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
     args = parser.parse_args()
 
     config = json.load(open(args.config_file))
